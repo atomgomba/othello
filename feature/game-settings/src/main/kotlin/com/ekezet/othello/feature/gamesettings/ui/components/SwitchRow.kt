2732f9b08e0dca61467d9e00a3a135eb88aafbb9
Kiripolszky Károly <karcsi@ekezet.com>
2023-10-11 08:19:37 +0200

    Make switch label clickable

--- feature/game-settings/src/main/kotlin/com/ekezet/othello/feature/gamesettings/ui/components/SwitchRow.kt
+++ feature/game-settings/src/main/kotlin/com/ekezet/othello/feature/gamesettings/ui/components/SwitchRow.kt
@@ -2,19 +2,26 @@ package com.ekezet.othello.feature.gamesettings.ui.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 
 @Composable
 internal fun SwitchRow(label: String, checked: Boolean, onCheckedChange: (value: Boolean) -> Unit) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.toggleable(
+            value = checked,
+            role = Role.Switch,
+            onValueChange = onCheckedChange,
+        )
     ) {
         Text(label)
         Spacer(Modifier.weight(1F))
-        Switch(checked = checked, onCheckedChange = onCheckedChange)
+        Switch(checked = checked, onCheckedChange = null)
     }
 }
