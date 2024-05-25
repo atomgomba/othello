b6b97fc622f059818643a8b7030ceaad3be6f8c1
Kiripolszky Károly <karcsi@ekezet.com>
2024-05-25 17:17:43 +0200

    Fix test

--- feature/game-board/src/test/kotlin/com/ekezet/othello/feature/gameboard/GameBoardEffectTest.kt
+++ feature/game-board/src/test/kotlin/com/ekezet/othello/feature/gameboard/GameBoardEffectTest.kt
@@ -4,19 +4,37 @@ import com.ekezet.hurok.test.EffectTest
 import com.ekezet.hurok.test.matches
 import com.ekezet.othello.core.data.models.Position
 import com.ekezet.othello.core.game.OthelloGameState
+import com.ekezet.othello.core.game.store.MoveHistoryStore
 import com.ekezet.othello.feature.gameboard.actions.OnGameEnded
 import com.ekezet.othello.feature.gameboard.actions.OnMoveMade
 import com.ekezet.othello.feature.gameboard.actions.OnTurnPassed
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class GameBoardEffectTest : EffectTest() {
+    @MockK
+    private lateinit var mockMoveHistoryStore: MoveHistoryStore
+
+    private lateinit var dependency: GameBoardDependency
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        dependency = GameBoardDependency(
+            moveHistoryStore = mockMoveHistoryStore,
+        )
+    }
+
     @Test
     fun `WaitBeforeNextTurn works correctly`() = runTest {
         val nextMove: Position = mockk()
 
-        Unit runWith WaitBeforeNextTurn(nextMove) matches {
+        dependency runWith WaitBeforeNextTurn(nextMove) matches {
             assertActions(listOf(OnMoveMade(nextMove)))
         }
     }
@@ -26,7 +44,7 @@ class GameBoardEffectTest : EffectTest() {
         val nextMove: Position = mockk()
         val newState: OthelloGameState = mockk()
 
-        Unit runWith WaitBeforePassTurn(nextMove, newState) matches {
+        dependency runWith WaitBeforePassTurn(nextMove, newState) matches {
             assertActions(listOf(OnTurnPassed(nextMove, newState)))
         }
     }
@@ -35,7 +53,7 @@ class GameBoardEffectTest : EffectTest() {
     fun `WaitBeforeGameEnd works correctly`() = runTest {
         val result: GameEnd = mockk()
 
-        Unit runWith WaitBeforeGameEnd(result) matches {
+        dependency runWith WaitBeforeGameEnd(result) matches {
             assertActions(listOf(OnGameEnded(result)))
         }
     }
