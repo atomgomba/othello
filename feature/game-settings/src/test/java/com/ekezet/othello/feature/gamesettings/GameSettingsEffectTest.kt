f72761f454992d0329191d1df7b895693fa17cf8
Kiripolszky Károly <karcsi@ekezet.com>
2024-06-01 15:27:10 +0200

    Fix GameSettingsEffectTest

--- feature/game-settings/src/test/java/com/ekezet/othello/feature/gamesettings/GameSettingsEffectTest.kt
+++ feature/game-settings/src/test/java/com/ekezet/othello/feature/gamesettings/GameSettingsEffectTest.kt
@@ -3,6 +3,7 @@ package com.ekezet.othello.feature.gamesettings
 import com.ekezet.hurok.test.EffectTest
 import com.ekezet.othello.core.game.data.Default
 import com.ekezet.othello.core.game.data.GameSettings
+import com.ekezet.othello.core.game.store.GameHistoryStore
 import com.ekezet.othello.core.game.store.GameSettingsStore
 import com.ekezet.othello.core.game.strategy.HumanPlayer
 import io.mockk.MockKAnnotations
@@ -12,12 +13,16 @@ import io.mockk.confirmVerified
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.runs
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
 import org.junit.Test
 
 internal class GameSettingsEffectTest : EffectTest() {
     @MockK
     private lateinit var mockGameSettingsStore: GameSettingsStore
+
+    @MockK
+    private lateinit var mockGameHistoryStore: GameHistoryStore
 
     private val initGameSettings = GameSettings.Default
 
@@ -29,19 +34,50 @@ internal class GameSettingsEffectTest : EffectTest() {
 
         dependency = GameGameSettingsDependency(
             gameSettingsStore = mockGameSettingsStore,
+            gameHistoryStore = mockGameHistoryStore,
         )
     }
 
     @Test
     fun `PublishGameSettings works correctly`() {
-        val settings = initGameSettings.copy(lightStrategy = HumanPlayer)
+        val settings = initGameSettings.copy()
 
         coEvery { mockGameSettingsStore.update(settings) } just runs
+        coEvery { mockGameSettingsStore.settings } returns MutableStateFlow(initGameSettings)
 
         dependency runWith PublishGameSettings(settings)
 
-        coVerify { mockGameSettingsStore.update(settings) }
+        coVerify {
+            mockGameSettingsStore.update(settings)
+            mockGameSettingsStore.settings
+        }
 
-        confirmVerified(mockGameSettingsStore)
+        confirmAllVerified()
+    }
+
+    @Test
+    fun `PublishGameSettings works correctly if strategy changed`() {
+        val settings = initGameSettings.copy(lightStrategy = HumanPlayer)
+
+        coEvery { mockGameSettingsStore.update(settings) } just runs
+        coEvery { mockGameSettingsStore.settings } returns MutableStateFlow(initGameSettings)
+        coEvery { mockGameHistoryStore.reset(any(), any()) } just runs
+
+        dependency runWith PublishGameSettings(settings)
+
+        coVerify {
+            mockGameSettingsStore.update(settings)
+            mockGameSettingsStore.settings
+            mockGameHistoryStore.reset(emptyList(), null)
+        }
+
+        confirmAllVerified()
+    }
+
+    private fun confirmAllVerified() {
+        confirmVerified(
+            mockGameSettingsStore,
+            mockGameHistoryStore,
+        )
     }
 }
