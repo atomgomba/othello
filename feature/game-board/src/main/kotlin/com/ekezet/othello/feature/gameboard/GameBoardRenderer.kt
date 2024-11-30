248d4f4a7f36bc3247344bed573d596378623f1d
atomgomba <karcsi@ekezet.com>
2024-11-30 13:45:15 +0100

    Show Pass label when viewing past turn

--- feature/game-board/src/main/kotlin/com/ekezet/othello/feature/gameboard/GameBoardRenderer.kt
+++ feature/game-board/src/main/kotlin/com/ekezet/othello/feature/gameboard/GameBoardRenderer.kt
@@ -2,6 +2,7 @@ package com.ekezet.othello.feature.gameboard
 
 import com.ekezet.hurok.Renderer
 import com.ekezet.othello.core.game.GameEnd.EndedWin
+import com.ekezet.othello.core.game.PastMove
 import com.ekezet.othello.core.ui.viewModels.toImmutableList
 import com.ekezet.othello.feature.gameboard.ui.viewModels.BoardOverlayList
 import com.ekezet.othello.feature.gameboard.ui.viewModels.OverlayItem.NextMoveIndicatorOverlayItem
@@ -12,6 +13,9 @@ import com.ekezet.othello.feature.gameboard.ui.viewModels.putAt
 import com.ekezet.othello.feature.gameboard.ui.viewModels.toImmutableList
 
 internal class GameBoardRenderer : Renderer<GameBoardModel, GameBoardDependency, GameBoardState> {
+    private val GameBoardModel.historicalMove: PastMove?
+        inline get() = if (isCurrentTurn) null else currentGameState.pastMoves.lastOrNull()
+
     override fun renderState(model: GameBoardModel) = with(model) {
         GameBoardState(
             board = currentGameState.board.toImmutableList(),
@@ -27,7 +31,7 @@ internal class GameBoardRenderer : Renderer<GameBoardModel, GameBoardDependency,
             nextMovePosition = nextMovePosition,
             displayOptions = boardDisplayOptions,
             ended = ended.takeIf { isCurrentTurn },
-            passed = passed,
+            passed = if (historicalMove != null) historicalMove!!.moveAt == null else passed,
             celebrate = isCurrentTurn && ended is EndedWin && ended.winner.isHumanPlayer,
             isHumanPlayer = currentGameState.currentDisk.isHumanPlayer,
             isCurrentTurn = isCurrentTurn,
@@ -47,12 +51,10 @@ internal class GameBoardRenderer : Renderer<GameBoardModel, GameBoardDependency,
         if (nextMovePosition != null) {
             items.putAt(nextMovePosition, NextMoveIndicatorOverlayItem(currentDisk))
         }
-        if (!isCurrentTurn) {
-            currentGameState.pastMoves.lastOrNull()?.let { pastMove ->
-                val moveAt = pastMove.moveAt
-                if (moveAt != null) {
-                    items.putAt(moveAt, PastMoveIndicatorOverlayItem(pastMove.disk))
-                }
+        historicalMove?.let { pastMove ->
+            val moveAt = pastMove.moveAt
+            if (moveAt != null) {
+                items.putAt(moveAt, PastMoveIndicatorOverlayItem(pastMove.disk))
             }
         }
         return items.toImmutableList()
