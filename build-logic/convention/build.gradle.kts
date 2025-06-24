34ecfea87e8ea715e227ac2d7ad9bd6c14e499b3
atomgomba <karcsi@ekezet.com>
2025-06-24 09:33:48 +0200

    chore: update Gradle, deps and up to API 36

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
 }
@@ -9,12 +7,6 @@ group = "com.ekezet.othello.buildlogic"
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
 }
 
 dependencies {
