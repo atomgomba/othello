b088030c7ac041cc304af9098a986789b874e978
atomgomba <karcsi@ekezet.com>
2025-06-24 09:48:09 +0200

    fix: Theme deprecation warning

--- core/ui/src/main/kotlin/com/ekezet/othello/core/ui/theme/Theme.kt
+++ core/ui/src/main/kotlin/com/ekezet/othello/core/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.ekezet.othello.core.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import android.view.Window
+import androidx.annotation.ColorInt
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -25,16 +27,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
 )
 
 @Composable
@@ -53,11 +45,12 @@ fun OthelloTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColorCompat = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
@@ -68,3 +61,17 @@ fun OthelloTheme(
         content = content,
     )
 }
+
+/**
+ * `statusBarColor` only has effect below Android 15
+ *
+ * Please see: [Behavior changes in Android 15](https://developer.android.com/about/versions/15/behavior-changes-15#ux)
+ */
+@Suppress("DEPRECATION")
+private var Window.statusBarColorCompat: Int
+    @ColorInt get() = statusBarColor
+    set(@ColorInt value) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            statusBarColor = value
+        }
+    }
