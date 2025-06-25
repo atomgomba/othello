9663f6662c76676e9bcb8fd527c592231a454ef7
atomgomba <karcsi@ekezet.com>
2025-06-25 10:27:02 +0200

    chore: update Detekt config

--- build-logic/convention/src/main/kotlin/com/ekezet/othello/Detekt.kt
+++ build-logic/convention/src/main/kotlin/com/ekezet/othello/Detekt.kt
@@ -1,25 +1,22 @@
 package com.ekezet.othello
 
-import com.android.build.api.dsl.CommonExtension
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import java.io.File
 
-internal fun Project.configureDetekt(
-    commonExtension: CommonExtension<*, *, *, *, *, *>,
-) {
-    commonExtension.apply {
-        with(pluginManager) {
-            apply(libs.findPlugin("detekt").get().get().pluginId)
-        }
-    }
-
-    configure<DetektExtension> {
-        autoCorrect = true
+internal fun Project.configureDetekt() {
+    with(pluginManager) {
+        apply(libs.findPlugin("detekt").get().get().pluginId)
     }
 
     dependencies {
         add("detektPlugins", libs.findLibrary("detekt-formatting").get())
+    }
+
+    configure<DetektExtension> {
+        autoCorrect = true
+        config.setFrom(File(rootProject.rootDir, "config/detekt/detekt.yml"))
     }
 }
