f77e62c8ac0527b3db2a62ddd31aed3b251e9af5
atomgomba <karcsi@ekezet.com>
2025-09-28 19:53:33 +0200

    fix: use default colors for TopAppBar

--- app/src/main/kotlin/com/ekezet/othello/main/ui/components/MainTopAppBar.kt
+++ app/src/main/kotlin/com/ekezet/othello/main/ui/components/MainTopAppBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -40,16 +39,13 @@ internal fun MainState.MainTopAppBar(
     }
 
     TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary,
-        ),
+        colors = TopAppBarDefaults.topAppBarColors(),
         title = { Text(titleString) },
         navigationIcon = {
             AnimatedVisibility(
                 visible = currentDestination != MainRoutes.Start,
             ) {
-                IconButton(onClick = { navController.popBackStack() }) {
+                IconButton(onClick = navController::popBackStack) {
                     Icon(
                         imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = stringResource(id = R.string.common__back),
