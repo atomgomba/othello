04cce4728a86afac0303f59bc3e64849f5058b09
atomgomba <karcsi@ekezet.com>
2025-10-13 17:31:09 +0200

    fix(GameBoard): respect correct proportions when drawing

--- core/ui/src/main/kotlin/com/ekezet/othello/core/ui/components/GameBoard.kt
+++ core/ui/src/main/kotlin/com/ekezet/othello/core/ui/components/GameBoard.kt
@@ -8,16 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
@@ -25,8 +19,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.ekezet.othello.core.data.models.BoardHeight
@@ -36,6 +37,7 @@ import com.ekezet.othello.core.data.models.Position
 import com.ekezet.othello.core.data.models.x
 import com.ekezet.othello.core.data.models.y
 import com.ekezet.othello.core.data.serialize.PositionLetters
+import com.ekezet.othello.core.data.serialize.PositionNumbers
 import com.ekezet.othello.core.game.GameEnd
 import com.ekezet.othello.core.game.GameEnd.EndedTie
 import com.ekezet.othello.core.game.GameEnd.EndedWin
@@ -46,7 +48,6 @@ import com.ekezet.othello.core.ui.viewModels.getAt
 
 private const val LOSER_ALPHA = 1 / 3F
 private const val CELL_WEIGHT = 1F / BoardWidth
-private val positionSize = 24.dp
 
 @ExperimentalLayoutApi
 @Composable
@@ -54,6 +55,7 @@ fun GameBoard(
     board: BoardList,
     modifier: Modifier = Modifier,
     background: Color = BoardBackground,
+    gaps: Color = MaterialTheme.colorScheme.background,
     boardCornerRadius: Dp = 16.dp,
     showPositions: Boolean = false,
     nextMovePosition: Position? = null,
@@ -66,6 +68,7 @@ fun GameBoard(
         GameBoardImpl(
             board = board,
             background = background,
+            gaps = gaps,
             boardCornerRadius = boardCornerRadius,
             showPositions = showPositions,
             nextMovePosition = nextMovePosition,
@@ -82,6 +85,7 @@ fun GameBoard(
 private fun GameBoardImpl(
     board: BoardList,
     background: Color,
+    gaps: Color,
     boardCornerRadius: Dp,
     showPositions: Boolean,
     nextMovePosition: Position?,
@@ -89,46 +93,115 @@ private fun GameBoardImpl(
     overlayFactory: (colIndex: Int, rowIndex: Int) -> Sprite?,
     isClickable: Boolean,
     onCellClick: (position: Position) -> Unit,
+    textMeasurer: TextMeasurer = rememberTextMeasurer(),
 ) {
-    Column {
-        for (rowIndex in 0 until BoardHeight) {
-            if (showPositions && rowIndex == 0) {
-                HorizontalPositions(nextMovePosition = nextMovePosition)
+    Box(
+        modifier = Modifier.run {
+            if (showPositions) {
+                drawPositions(
+                    textMeasurer = textMeasurer,
+                    color = MaterialTheme.colorScheme.outline,
+                    nextMovePosition = nextMovePosition,
+                )
+            } else {
+                this
             }
+        },
+    ) {
+        Column(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(boardCornerRadius))
+                .background(background)
+                .drawGaps(color = gaps),
+        ) {
+            for (rowIndex in 0 until BoardHeight) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    for (colIndex in 0 until BoardWidth) {
+                        val disk = board.getAt(colIndex, rowIndex)
+                        val overlayItem = overlayFactory(colIndex, rowIndex)
 
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                for (colIndex in 0 until BoardWidth) {
-                    val disk = board.getAt(colIndex, rowIndex)
-                    val overlayItem = overlayFactory(colIndex, rowIndex)
-
-                    if (showPositions && colIndex == 0) {
-                        VerticalPosition(number = rowIndex + 1, nextMovePosition = nextMovePosition)
+                        key(colIndex, rowIndex, disk != null, overlayItem?.composeKey, ended) {
+                            BoardCell(
+                                disk = disk,
+                                overlayItem = overlayItem,
+                                colIndex = colIndex,
+                                rowIndex = rowIndex,
+                                ended = ended,
+                                isClickable = isClickable,
+                                onCellClick = onCellClick,
+                            )
+                        }
                     }
-
-                    key(colIndex, rowIndex, disk != null, overlayItem?.composeKey, ended) {
-                        BoardCell(
-                            disk = disk,
-                            overlayItem = overlayItem,
-                            colIndex = colIndex,
-                            rowIndex = rowIndex,
-                            background = background,
-                            boardCornerRadius = boardCornerRadius,
-                            ended = ended,
-                            isClickable = isClickable,
-                            onCellClick = onCellClick,
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.size(1.dp))
                 }
             }
-
-            Spacer(modifier = Modifier.size(1.dp))
         }
     }
 }
+
+private fun Modifier.drawGaps(color: Color) = this then Modifier
+    .drawWithCache {
+        onDrawWithContent {
+            val stepX = size.width / BoardWidth
+            val stepY = size.height / BoardHeight
+            val strokeWidth = 2F
+
+            drawContent()
+            for (n in 1.until(BoardWidth)) {
+                drawLine(
+                    color = color,
+                    start = Offset(n * stepX, 0F),
+                    end = Offset(n * stepX, size.height),
+                    strokeWidth = strokeWidth,
+                )
+                drawLine(
+                    color = color,
+                    start = Offset(0F, n * stepY),
+                    end = Offset(size.width, n * stepY),
+                    strokeWidth = strokeWidth,
+                )
+            }
+        }
+    }
+
+private fun Modifier.drawPositions(
+    textMeasurer: TextMeasurer,
+    color: Color,
+    nextMovePosition: Position?,
+) = this then Modifier
+    .padding(start = 24.dp, top = 24.dp)
+    .drawWithCache {
+        onDrawWithContent {
+            val stepX = size.width / BoardWidth
+            val stepY = size.height / BoardHeight
+            val startX = 48F
+            val startY = 32F
+            val offsetX = -56F
+            val offsetY = -68F
+            val style = TextStyle(color = color)
+
+            drawContent()
+            for (x in 0.until(BoardWidth)) {
+                drawText(
+                    textMeasurer = textMeasurer,
+                    text = "${PositionLetters[x]}",
+                    topLeft = Offset(startX + x * stepX, offsetY),
+                    size = Size(stepX, 48F),
+                    style = if (x == nextMovePosition?.x) style.copy(fontWeight = FontWeight.Bold) else style,
+                )
+            }
+            for (y in 0.until(BoardHeight)) {
+                drawText(
+                    textMeasurer = textMeasurer,
+                    text = "${PositionNumbers[y]}",
+                    topLeft = Offset(offsetX, startY + y * stepY),
+                    size = Size(48F, stepY),
+                    style = if (y == nextMovePosition?.y) style.copy(fontWeight = FontWeight.Bold) else style,
+                )
+            }
+        }
+    }
 
 @Composable
 private fun RowScope.BoardCell(
@@ -136,16 +209,12 @@ private fun RowScope.BoardCell(
     overlayItem: Sprite?,
     colIndex: Int,
     rowIndex: Int,
-    background: Color,
-    boardCornerRadius: Dp,
     ended: GameEnd?,
     isClickable: Boolean,
     onCellClick: (position: Position) -> Unit,
 ) {
     Box(
         modifier = Modifier
-            .roundedCell(colIndex, rowIndex, boardCornerRadius)
-            .background(color = background)
             .weight(CELL_WEIGHT)
             .aspectRatio(1F)
             .clickable(enabled = isClickable) {
@@ -171,53 +240,4 @@ private fun RowScope.BoardCell(
 
         overlayItem?.Composable()
     }
-}
-
-@Composable
-private fun HorizontalPositions(
-    nextMovePosition: Position?,
-) {
-    Row(
-        modifier = Modifier
-            .padding(start = positionSize)
-            .fillMaxWidth()
-            .height(positionSize),
-    ) {
-        for ((i, letter) in PositionLetters.withIndex()) {
-            Text(
-                text = "$letter",
-                textAlign = TextAlign.Center,
-                color = if (nextMovePosition?.x == i) Color.Unspecified else MaterialTheme.colorScheme.outlineVariant,
-                modifier = Modifier.weight(CELL_WEIGHT),
-            )
-        }
-    }
-}
-
-@ExperimentalLayoutApi
-@Composable
-private fun VerticalPosition(
-    number: Int,
-    nextMovePosition: Position?,
-) {
-    Column {
-        Box {
-            Text(
-                text = "$number",
-                textAlign = TextAlign.Center,
-                color = if (nextMovePosition?.y == number - 1) Color.Unspecified else MaterialTheme.colorScheme.outlineVariant,
-                modifier = Modifier
-                    .width(positionSize)
-                    .align(Alignment.Center),
-            )
-        }
-    }
-}
-
-private fun Modifier.roundedCell(x: Int, y: Int, radius: Dp) = when {
-    x == 0 && y == 0 -> clip(shape = RoundedCornerShape(topStart = radius))
-    x == 7 && y == 0 -> clip(shape = RoundedCornerShape(topEnd = radius))
-    x == 0 && y == 7 -> clip(shape = RoundedCornerShape(bottomStart = radius))
-    x == 7 && y == 7 -> clip(shape = RoundedCornerShape(bottomEnd = radius))
-    else -> this
 }
